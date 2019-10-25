aa599c900682b59a4112f671ec5cb607768c7935
Tarulah <taru.lahtinen@metropolia.fi>
2019-10-25 15:17:42 +0300

    Base Set lisätty database kansioon

---
+++ pokemonkorttijarjestelma/database/BaseSetCreateAndFill.sql
@@ -0,0 +1,113 @@
+CREATE TABLE BaseSet(
+	id INT IDENTITY(1, 1) PRIMARY KEY,
+	cardName VARCHAR(255) NOT NULL,
+	setNumber VARCHAR(10) NOT NULL,
+	PokemonType VARCHAR(20) NOT NULL,
+	cardType VARCHAR(20) NOT NULL,
+	other VARCHAR(255),
+	photo VARCHAR(255)
+);
+
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Alakazam', '1/102', 'Psychic', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Blastoise', '2/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Chansey', '3/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Charizard', '4/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Clefairy', '5/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Gyarados', '6/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Hitmonchan', '7/102', 'Fighting', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Machamp', '8/102', 'Fighting', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Magneton', '9/102', 'Lightning', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Mewtwo', '10/102', 'Psychic', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Nidoking', '11/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Ninetales', '12/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Poliwrath', '13/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Raichu', '14/102', 'Lightning', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Venusaur', '15/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Zapdos', '16/102', 'Lightning', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Beedrill', '17/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Dragonair', '18/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Dugtrio', '19/102', 'Fighting', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Electabuzz', '20/102', 'Lightning', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Electrode', '21/102', 'Lightning', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Pidgeotto', '22/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Arcanine', '23/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Charmeleon', '24/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Dewgong', '25/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Dratini', '26/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Farfetch\'d', '27/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Growlithe', '28/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Haunter', '29/102', 'Psychic', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Ivysaur', '30/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Jynx', '31/102', 'Psychic', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Kadabra', '32/102', 'Psychic', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Kakuna', '33/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Machoke', '34/102', 'Fighting', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Magikarp', '35/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Magmar', '36/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Nidorino', '37/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Poliwhirl', '38/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Porygon', '39/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Raticate', '40/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Seel', '41/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Wartortle', '42/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Abra', '43/102', 'Psychic', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Bulbasaur', '44/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Caterpie', '45/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Charmander', '46/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Diglett', '47/102', 'Fighting', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Doduo', '48/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Ghastly', '50/102', 'Psychic', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Koffing', '51/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Machop', '52/102', 'Fighting', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Magnemite', '53/102', 'Lightning', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Metapod', '54/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Nidoran (m)', '55/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Onix', '56/102', 'Fighting', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Pidgey', '57/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Pikachu', '58/102', 'Lightning', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Poliwag', '59/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Ponyta', '60/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Rattata', '61/102', 'Colorless', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Sandshrew', '62/102', 'Fighting', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Squirtle', '63/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Starmie', '64/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Staryu', '65/102', 'Water', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Tangela', '66/102', 'Grass', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Voltorb', '67/102', 'Lightning', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Vulpix', '68/102', 'Fire', 'Pokemon');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Weedle', '69/102', 'Grass', 'Pokemon');
+
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Clefairy Doll', '70/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Computer Search', '71/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Devolution Spray', '72/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Imposter Professor Oak', '73/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Item Finder', '74/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Lass', '75/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Pokémon Breeder', '76/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Pokémon Trader', '77/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Scoop Up', '78/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Super Energy Removal', '79/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Defender', '80/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Energy Retrieval', '81/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Full Heal', '82/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Maintenance', '83/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('PlusPower', '84/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Pokémon Center', '85/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Pokémon Flute', '86/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Pokédex', '87/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Professor Oak', '88/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Revive', '89/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Super Potion',	'90/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Bill', '91/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Energy Removal', '92/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Gust of Wind', '93/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Potion', 					'94/102', 'Trainer', 'Trainer');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Switch', '95/102', 'Trainer', 'Trainer');
+
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Double Colorless Energy', '96/102', 'Colorless', 'Energy');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Fighting Energy', '97/102', 'Fighting', 'Energy');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Fire Energy', '98/102', 'Fire', 'Energy');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Grass Energy', '99/102', 'Grass', 'Energy');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Lightning Energy', '100/102', 'Lightning', 'Energy');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Psychic Energy', '101/102', 'Psychic', 'Energy');
+INSERT INTO BaseSet(cardName, setNumber, PokemonType, cardType) VALUES ('Water Energy', '102/102', 'Water', 'Energy');
