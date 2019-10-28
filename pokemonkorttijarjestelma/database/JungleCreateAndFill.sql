6fd3d651dc02e10a050e962c864e4b955b1dd5b1
annaksi <akp.hakala@gmail.com>
2019-10-28 09:33:52 +0200

    Lisätty Jungle database info

---
+++ pokemonkorttijarjestelma/database/JungleCreateAndFill.sql
@@ -0,0 +1,76 @@
+CREATE TABLE Jungle(
+	id INT IDENTITY(1, 1) PRIMARY KEY,
+	cardName VARCHAR(255) NOT NULL,
+	setNumber VARCHAR(10) NOT NULL,
+	PokemonType VARCHAR(20) NOT NULL,
+	cardType VARCHAR(20) NOT NULL,
+	other VARCHAR(255),
+	photo VARCHAR(255)
+);
+
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Cleafable', '1/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Electrode', '2/64', 'Lightning', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Flareon', '3/64', 'Fire', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Jolteon', '4/64', 'Lightning', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Kangaskhan', '5/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Mr. Mime', '6/64', 'Psychic', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Nidoqueen', '7/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Pidgeot', '8/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Pinsir', '9/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Scyther', '10/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Snorlax', '11/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Vaporeon', '12/64', 'Water', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Venomoth', '13/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Victreebel', '14/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Vileplume', '15/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Wigglytuff', '16/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Clefable', '17/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Electrode', '18/64', 'Lightning', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Flareon', '19/64', 'Fire', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Jolteon', '20/64', 'Lightning', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Kangaskhan', '21/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Mr. Mime', '22/64', 'Psychic', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Nidoqueen', '23/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Pidgeot', '24/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Pinsir', '25/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Scyther', '26/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Snorlax', '27/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Vaporeon', '28/64', 'Water', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Venomoth', '29/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Victreebel', '30/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Vileplume', '31/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Wigglytuff', '32/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Butterfree', '33/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Dodrio', '34/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Exeggutor', '35/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Fearow', '36/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Gloom', '37/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Lickitung', '38/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Marowak', '39/64', 'Fighting', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Nidorina', '40/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Parasect', '41/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Persian', '42/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Primeape', '43/64', 'Fighting', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Rapidash', '44/64', 'Fire', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Rhydon', '45/64', 'Fighting', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Seaking', '46/64', 'Water', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Tauros', '47/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Weepinbell', '48/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Bellsprout', '49/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Cubone', '50/64', 'Fighting', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Eevee', '51/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Exeggcute', '52/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Goldeen', '53/64', 'Water', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Jigglypuff', '54/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Mankey', '55/64', 'Fighting', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Meowth', '56/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Nidoran (f)', '57/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Oddish', '58/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Paras', '59/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Pikachu', '60/64', 'Lightning', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Rhyhorn', '61/64', 'Fighting', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Spearow', '62/64', 'Colorless', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Venonat', '63/64', 'Grass', 'Pokemon');
+INSERT INTO Jungle(cardName, setNumber, PokemonType, cardType) VALUES ('Poké Ball', '64/64', 'Trainer', 'Trainer');
+
+select * from Jungle;
