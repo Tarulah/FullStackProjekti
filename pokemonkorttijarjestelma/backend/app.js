00a1911d6e56fa19320dcc205547162d908d414a
Tarulah <taru.lahtinen@metropolia.fi>
2019-11-01 12:00:16 +0200

    Paikallinen SQL yhteys gettiin tehty

--- pokemonkorttijarjestelma/backend/app.js
+++ pokemonkorttijarjestelma/backend/app.js
@@ -1,35 +1,46 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mssql = require("mssql");
 
 let app = express();
 
-//Take stuff from database and put it into json file
-
-//Dummy Database
-let database = []
-let id = 100
-
 app.use(bodyParser.json());
 
+let cardset = "BaseSet";
+
 //GET
-//testi
-app.get("/testi", function(req, res) {
-	return res.status(200).json(database);
+app.get("/cards/" + cardset, function(req, res) {
+	
+	let config = {
+		user: '',
+		password: '',
+		server: 'localhost',
+		database: 'pokemonkorttisql'
+	};
+	
+	mssql.connect(config, function(err){
+		if(err){
+			console.log("Error while connecting to database: " + err);
+			return res.status(404).json({message:"not found"});
+		}
+		
+		let request = new mssql.Request();
+		
+		request.query('select * from ' + cardset, function(err, recordset){
+			if(err){
+				console.log("Error with the query: " + err);
+				return res.status(404).json({message:"not found"});
+			}
+			
+			mssql.close();
+			return res.status(200).json(recordset);
+		});
+	});
 });
 
 //POST
 app.post("/testi",function(req, res) {
-	let card = {
-		id:id,
-		cardName:req.body.cardName,
-		setNumber:req.body.setNumber,
-		pokemonType:req.body.pokemonType,
-		cardType:req.body.cardType
-	}
-	
-	database.push(card);
-	id++;
-	return res.status(200).json({message:"success"});
+
 });
 
 //DELETE
