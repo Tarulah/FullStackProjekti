ff07c114cfad6e31e93741825121686389592b2c
Tarulah <taru.lahtinen@metropolia.fi>
2019-11-08 10:13:02 +0200

    index.js konflikti korjattu

--- pokemonkorttijarjestelma/frontend/src/index.js
+++ pokemonkorttijarjestelma/frontend/src/index.js
@@ -6,16 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-<<<<<<< HEAD
 <BrowserRouter>
     <App />
 </BrowserRouter>
 , document.getElementById('root'));
-=======
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>, document.getElementById('root'));
->>>>>>> fbd894f0efbeca56ef876295ab057c23d913a05b
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
