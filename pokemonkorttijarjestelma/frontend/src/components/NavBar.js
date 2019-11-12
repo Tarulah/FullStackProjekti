2f6367ee04de64136aa7edcddf9bd2f93c7627d2
annaksi <akp.hakala@gmail.com>
2019-11-12 15:46:12 +0200

    NavBar.js päivitys
    Muoto alkaa pikkuhiljaa löytymään...

--- pokemonkorttijarjestelma/frontend/src/components/NavBar.js
+++ pokemonkorttijarjestelma/frontend/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
-import { List, Header, Button } from 'semantic-ui-react';
+import { Header, Button, Form } from 'semantic-ui-react';
 import Nav from 'react-bootstrap/Nav';
 
 
@@ -18,21 +18,23 @@ export default class NavBar extends React.Component {
         }
 
         let style = {
-            height: 100,
+            height: 50,
             backgroundColor: "blue",
-            padding: 10
+            padding: 15
         }
 
         if (this.props.isLogged) {
             return (
                 <div style={style}>
-                    <Nav activeKey="/home"
-                    onSelect={selectedKey => alert(`selected ${selectedKey}`)}>                    
+                    <Nav >                    
                         <Nav.Item><Link to="/sets">Sets</Link></Nav.Item>
-                        <Nav.Item><Link to="/info">Info</Link></Nav.Item>              
-                        <Button onClick={this.logout}
-                        class="ulos" name="login">Log Out</Button>
-                    </Nav>
+                        <Nav.Item><Link to="/info">Info</Link></Nav.Item>                  
+                        </Nav>
+                    <Form >
+                    <Button onClick={this.logout}
+                        size="sm">Log Out</Button>
+                    </Form>
+                    
                 </div>
             )
         } else {
