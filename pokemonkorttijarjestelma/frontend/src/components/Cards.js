35d00bdc74c1c0fb5ab88403faf826ebd14d2ea8
annaksi <akp.hakala@gmail.com>
2019-11-13 14:58:31 +0200

    Cards.js päivitys
    Kortti-sivu lisätty, kaksi vaihtoehtoa, ilman kuvaa

--- pokemonkorttijarjestelma/frontend/src/components/Cards.js
+++ pokemonkorttijarjestelma/frontend/src/components/Cards.js
@@ -2,34 +2,73 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
+
 export default class Cards extends React.Component {
 
-render() {
-    return (
-        <div>
-        <Container>
-            <Row classname="justify-content-md-center">
-                <Col xs lg="2">
-                    <img src={""} alt=""></img>                       
-                </Col>
-                <Col md="auto"></Col>
-                <Col xs lg="2">
-                    <h4>Clefable</h4>
-                </Col>
-
-            </Row>
-            <Row classname="justify-content-md-center">
-                <Col xs lg="2">
-                <img src={""} alt=""></img>
-                </Col>
-                <Col md="auto"></Col>
-                <Col xs lg="2">
-                    <h4>Electrode</h4>
-                </Col>
-            </Row>
-            </Container>
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row class="justify-content-xs-center">
+                        <Col xs="2">
+                            <img src={""} alt=""></img>
+                        </Col>
+                        <Col xs="2">
+                            <Row>
+                                <h4>Jungle</h4>
+                            </Row>
+                            <Row>
+                                <p>Clefable</p>
+                            </Row>
+                            <Row>
+                                <p>1/64</p>
+                            </Row>
+                            <Row>
+                                <p>holo</p>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <br></br>
+                    <Row class="justify-content-xs-center">
+                        <Col xs="2">
+                            <img src={""} alt=""></img>
+                        </Col>
+                        <Col xs="2">
+                            <Row>
+                                <h4>Jungle</h4>
+                            </Row>
+                            <Row>
+                                <p>Clefable</p>
+                            </Row>
+                            <Row>
+                                <p>1/64</p>
+                            </Row>
+                            <Row>
+                                <p>holo</p>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <hr></hr>
+                    <div class="d-flex justify-content-center">
+                        <Col xs="2"><img src={""} alt=""></img></Col>
+                        <Col xs="2">
+                            <Row>
+                                <h4>Jungle</h4>
+                            </Row>
+                            <Row>
+                                <p>Clefable</p>
+                            </Row>
+                            <Row>
+                                <p>1/64</p>
+                            </Row>
+                            <Row>
+                                <p>holo</p>
+                            </Row>
+                        </Col>
+                    </div>
+                </Container>
             </div>
-    )
-}   
-} 
+        )
+    }
+}
 
